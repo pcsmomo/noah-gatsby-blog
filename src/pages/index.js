7187cc3305b464ae67237046e72e3745643d6393
pcsmomo <dongwoo_333@hanmail.net>
2021-06-13 11:18:36 +1000

    adding all our new blog code for gatsby setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,65 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+const IndexPage = ({ data }) => {
+  const { totalCount, edges } = data.allMarkdownRemark
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div>
+        <h1>Noah's Thoughts</h1>
+        <h4>{totalCount}</h4>
+        {edges.map(({ node }) => {
+          const { id, frontmatter, excerpt, fields } = node
+          return (
+            <div key={id}>
+              <BlogLink to={fields.slug}>
+                <BlogTitle>
+                  {frontmatter.title} - {frontmatter.date}
+                </BlogTitle>
+              </BlogLink>
+              <p>{excerpt}</p>
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
